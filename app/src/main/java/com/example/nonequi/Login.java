e4291be5fa66741181cf28a513d46cde07fb1ff3
Cristian Coronel Job <cristiancamilocoronelc800@gmail.com>
2022-11-28 15:38:31 -0500

    SplashScreen complete, a little bit

--- app/src/main/java/com/example/nonequi/Login.java
+++ app/src/main/java/com/example/nonequi/Login.java
@@ -2,9 +2,15 @@ package com.example.nonequi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AnticipateInterpolator;
+
 import androidx.core.splashscreen.SplashScreen;
 
 
@@ -15,6 +21,30 @@ public class Login extends AppCompatActivity {
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            getSplashScreen().setOnExitAnimationListener(splashScreenView -> {
+                final ObjectAnimator slideUp = ObjectAnimator.ofFloat(
+                        splashScreenView,
+                        View.TRANSLATION_Y,
+                        0f,
+                        -splashScreenView.getHeight()
+                );
+                slideUp.setInterpolator(new AnticipateInterpolator());
+                slideUp.setDuration(350L);
+
+                slideUp.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        splashScreenView.remove();
+                    }
+                });
+
+                slideUp.start();
+            });
+        }
+
+        splashScreen.setKeepOnScreenCondition(() -> false );
     }
 
     public void signIn(View view) {

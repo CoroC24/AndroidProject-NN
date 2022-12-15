489f8b5419315a348a9fc4bedaa666d4c78b12ae
Cristian Coronel Job <cristiancamilocoronelc800@gmail.com>
2022-12-15 15:27:19 -0500

    Changes in interface history

--- app/src/main/java/Interfaces/History/HistoryTransactionListAdapter.java
+++ app/src/main/java/Interfaces/History/HistoryTransactionListAdapter.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.nonequi.DBConnection;
 import com.example.nonequi.R;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
